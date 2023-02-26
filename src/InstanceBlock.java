484bda7ebca522b8b80321ab4817022bdfe505d4
om <om@Sumeet>
2023-02-26 23:56:02 +0530

    hi this is my 1st commit

---
+++ src/InstanceBlock.java
@@ -0,0 +1,29 @@
+
+class InstanceBlock
+{
+     static int a=30;
+      static int b=40;
+     
+     static{
+    	 
+    	  System.out.println("Value of b = " +b);
+    	}
+
+public static void main(String[] args) 
+{
+  // InstanceBlock i=new InstanceBlock();
+{
+
+System.out.println("Value of a = " +a);
+}	
+
+
+ 
+}
+
+
+}
+
+
+
+
---
+++ src/InstanceBlock1.java
@@ -0,0 +1,28 @@
+
+
+public class InstanceBlock1 {
+	 
+          static int a=30;
+	      static int b=40;
+	      static {
+	          
+	          System.out.println("Value of b = " +b);
+	     }
+	 public static void main(String[] args) 
+{
+        //InstanceBlock1 i=new InstanceBlock1();
+  {
+
+    System.out.println("Value of a = " +a);
+  }	
+
+
+ 
+}
+	
+
+}
+
+
+
+
